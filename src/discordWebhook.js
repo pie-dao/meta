69826031c9804fdf647bd4575a2ff3f58f396b64
Dan Matthews <dan@cryptobuilders.io>
2020-05-14 17:09:36 +0300

    add discord webhook management

---
+++ src/discordWebhook.js
@@ -0,0 +1,53 @@
+import fetch from 'node-fetch';
+
+import { exec } from 'child_process';
+import { validateIsString } from '@pie-dao/utils';
+
+const repo = process.env.REPO;
+const token = process.env.TOKEN;
+const webhook = process.env.WEBHOOK;
+
+validateIsString(repo, {
+  message: 'Please set environment variable REPO. Ex: REPO="pie-dao/meta" yarn branchProtection"',
+});
+
+validateIsString(token, {
+  message: 'Please set environment variable TOKEN. Ex: TOKEN="<token>" yarn branchProtection"',
+});
+
+validateIsString(webhook, {
+  message: 'Please set environment variable WEBHOOK. ' +
+    'Ex: WEBHOOK="https://discordapp.com/api/webhooks/..." yarn discordWebhook"',
+});
+
+const main = async () => {
+  let url = `https://api.github.com/repos/${repo}/hooks`;
+  const headers = { 'Authorization': `token ${token}` };
+
+  const response1 = await fetch(url, { headers });
+  const hooks = await response1.json();
+
+  let exists = false;
+  let method = 'POST';
+
+  hooks.forEach((hook) => {
+    if (hook.config.url === webhook) {
+      exists = hook.id;
+      method = 'PATCH';
+      url = `${url}/${hook.id}`;
+    }
+  });
+
+  const body = JSON.stringify({
+    active: true,
+    config: { url: webhook, content_type: 'json' },
+    events: [ '*' ],
+    name: 'web',
+  });
+
+  console.log(method, url);
+  const response2 = await fetch(url, { body, headers, method });
+  console.log(await response2.json());
+};
+
+main();
