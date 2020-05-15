ce47d172979d01d2df4a9a6b9e3dea107f0b4201
Dan Matthews <dan@cryptobuilders.io>
2020-05-15 15:01:04 +0300

    adds linter setup with github action for linting pull requests

---
+++ config/eslint/react.eslint.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': 0,
+    'no-underscore-dangle': 0,
+    'react/jsx-filename-extension': 0,
+  },
+};
---
+++ src/utils/run.js
@@ -0,0 +1,25 @@
+import { exec } from 'child_process';
+
+const run = (commands) => new Promise((resolve, reject) => {
+  console.log('Running:\n');
+  commands.forEach((command) => {
+    console.log(command);
+  });
+
+  exec(commands.join(';'), (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      resolve();
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      resolve();
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    resolve();
+  });
+});
+
+export default run;
