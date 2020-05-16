6918a64c3bdaba152b40ded65bda07a21e146d89
Dan Matthews <dan@cryptobuilders.io>
2020-05-16 03:40:25 +0300

    add linting configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,26 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'airbnb-base',
+    'plugin:react/recommended',
+    'airbnb',
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: [
+    'react',
+  ],
   rules: {
     'no-console': 0,
     'no-underscore-dangle': 0,
+    'react/jsx-filename-extension': 0,
   },
 };
